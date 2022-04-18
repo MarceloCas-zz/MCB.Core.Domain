0cd62e92041abbb6704e28e339d036982ffb4ecf
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-18 01:55:10 -0300

    feat: add projects

---
+++ tst/MCB.Core.Domain.Tests/DummyTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace MCB.Core.Domain.Tests
+{
+    public class DummyTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
