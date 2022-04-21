f128320ae4ede8cf0129ee865435187825771c5e
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-21 02:13:41 -0300

    feat: add repository base

---
+++ src/MCB.Core.Domain/Repositories/RepositoryBase.cs
@@ -0,0 +1,11 @@
+﻿using MCB.Core.Domain.Abstractions.Repositories;
+using MCB.Core.Domain.Entities.Abstractions;
+
+namespace MCB.Core.Domain.Repositories
+{
+    public abstract class RepositoryBase<TAggregationRoot>
+        : IRepository<TAggregationRoot>
+        where TAggregationRoot : IAggregationRoot
+    {
+    }
+}
