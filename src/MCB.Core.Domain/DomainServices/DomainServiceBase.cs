335f19f17279c3d376f575020d23f5ebd3647f4f
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-21 02:12:01 -0300

    feat: add domain service base

---
+++ src/MCB.Core.Domain/DomainServices/DomainServiceBase.cs
@@ -0,0 +1,11 @@
+﻿using MCB.Core.Domain.Abstractions.DomainServices;
+using MCB.Core.Domain.Entities.Abstractions;
+
+namespace MCB.Core.Domain.DomainServices
+{
+    public abstract class DomainServiceBase<TAggregationRoot>
+        : IDomainService<TAggregationRoot>
+        where TAggregationRoot : IAggregationRoot
+    {
+    }
+}
