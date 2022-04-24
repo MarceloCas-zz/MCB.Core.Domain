76ce568a624728f20380368b69155bd4b4b1e40d
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-23 22:34:48 -0300

    test: change boolean expression to nested if statements

--- src/MCB.Core.Domain/DomainEntities/Specifications/DomainEntitySpecificationsBase.cs
+++ src/MCB.Core.Domain/DomainEntities/Specifications/DomainEntitySpecificationsBase.cs
@@ -111,11 +111,53 @@ namespace MCB.Core.Domain.DomainEntities.Specifications
         {
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
             validator.RuleFor(domainEntity => domainEntity)
-                .Must(domainEntity =>
-                    domainEntity.AuditableInfo.UpdatedAt >= domainEntity.AuditableInfo.CreatedAt
-                    && domainEntity.AuditableInfo.UpdatedAt < DateTimeOffset.UtcNow
-                    && domainEntity.AuditableInfo.UpdatedBy.Length <= 250
-                )
+                .Must(domainEntity => {
+
+                    if(domainEntity.AuditableInfo.UpdatedAt < DateTimeOffset.UtcNow)
+                    {
+                        if (domainEntity.AuditableInfo.UpdatedAt > domainEntity.AuditableInfo.CreatedAt)
+                        {
+                            if(domainEntity.AuditableInfo.UpdatedBy.Length < 250)
+                            {
+                                return true;
+                            }
+                            else if(domainEntity.AuditableInfo.UpdatedBy.Length == 250)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }    
+                        }
+                        else if(domainEntity.AuditableInfo.UpdatedAt == domainEntity.AuditableInfo.CreatedAt)
+                        {
+                            if (domainEntity.AuditableInfo.UpdatedBy.Length < 250)
+                            {
+                                return true;
+                            }
+                            else if (domainEntity.AuditableInfo.UpdatedBy.Length == 250)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    //domainEntity.AuditableInfo.UpdatedAt >= domainEntity.AuditableInfo.CreatedAt
+                    //    && domainEntity.AuditableInfo.UpdatedAt < DateTimeOffset.UtcNow
+                    //    && domainEntity.AuditableInfo.UpdatedBy.Length <= 250
+                })
                 .When(domainEntity => CheckUpdateInfoIsRequired(domainEntity))
                 .WithSeverity(UPDATE_INFO_SHOULD_BE_VALID_ERROR_SEVERITY)
                 .WithErrorCode(UPDATE_INFO_SHOULD_BE_VALID_ERROR_CODE)
